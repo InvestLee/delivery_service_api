30fefac74a5ee4001677dedd28dc484d91900d9d
InvestLee <dlwlgud216@nate.com>
2024-04-02 23:34:48 +0900

    Api 공통 Spec 적용

--- service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -2,12 +2,14 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -18,12 +20,14 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me(){
+    public Api<AccountMeResponse> me(){
 
-        return AccountMeResponse.builder()
+        var response = AccountMeResponse.builder()
                 .name("이지형")
                 .email("dlwlgud216@nate.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+
+        return Api.ok(response);
     }
 }
